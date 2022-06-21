2b6cc185d62f6950894f3dc3164031cd781a76d4
yo-sarawut <yo_sarawut@hotmail.com>
2022-06-22 00:27:13 +0700

    add data

--- streamlit_app.py
+++ streamlit_app.py
@@ -198,7 +198,7 @@ with col2:
 with col3:
     st.write("")
 with col4:
-    price = st.slider('52 Week Range', f'{fiftyTwoWeekLow}, {fiftyTwoWeekHigh},{currentPrice}')
+    price = st.slider('52 Week Range', fiftyTwoWeekLow, fiftyTwoWeekHigh,currentPrice)
 with col5:
     st.write("")
 
